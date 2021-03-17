11ea1cfcbb71bf56847220140f376174edd8c9a3
Timothy Luna <timothy.d.luna@gmail.com>
2021-03-17 17:01:21 -0500

    Pushing types

---
+++ src/utils/types.py
@@ -0,0 +1,11 @@
+from ray import ObjectId
+from typing import Union, Iterable
+
+ActionID = str
+Action = Union[ObjectId,ActionID] (inherited either from Ray or PyArrow, shouldn't matter)
+ActionSet = Iterable[Action]
+ExperimentID = str
+Experiment = Union[ObjectId,ExperimentID]
+ActionBank: Iterable[Action]
+AgentID = str
+Agent = Union[ObjectId,AgentID]

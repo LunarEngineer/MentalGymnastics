abbe0ad5c7202d5377108a8935223fc8606b67bc
Timothy Luna <timothy.d.luna@gmail.com>
2021-04-07 18:20:58 -0500

    initial action bank commit

---
+++ src/mentalgym/actionbank.py
@@ -0,0 +1,16 @@
+"""Contains the action bank class and supporting code."""
+
+class ActionBank():
+    """Builds and tracks actions and their history.
+
+    """
+
+    def __init__(self):
+        raise NotImplementedError
+    
+    def query(self, action_id:str):
+        """Return action information.
+
+        Thi
+
+        """

2b05528b694ca74ae287ba706491bebd1408dc48
Timothy Luna <timothy.d.luna@gmail.com>
2021-04-08 06:58:37 -0500

    chore(actionbank): put some work towards action bank API

--- src/mentalgym/actionbank.py
+++ src/mentalgym/actionbank.py
@@ -1,4 +1,7 @@
 """Contains the action bank class and supporting code."""
+import pandas as pd
+from __futures__ import annotation
+from utils.types import Action, ActionSet
 
 class ActionBank():
     """Builds and tracks actions and their history.
@@ -26,6 +29,8 @@ class ActionBank():
     -------
     query()
         Returns information for queried actions
+    _query()
+        Calls .query on the Pandas representation of the actions.
     sample()
         Returns a sample of actions
     prune()
@@ -43,28 +48,93 @@ class ActionBank():
     def __init__(
         self,
         action_bank_directory: str = ".action_bank"
-    ):
+    ) -> ActionBank:
         self._action_bank_directory = action_bank_directory
-        self._build_bank(action_bank_directory)
+        self._action_manifest = self._build_bank()
         raise NotImplementedError
 
-    def _build_bank(self):
+    def _build_bank(self) -> ActionSet:
         """Build action bank from local directory.
 
-        Read a json document to build the action_manifest dict.
+        Read a json document to build the action manifest.
 
         This function can get smarter, but this works as a stopgap.
 
         Returns
         -------
-        action_manifest: Dict[str,Dict[str,Any]]
+        action_manifest: ActionSet
             This is an action manifest
         """
+	bank = None
+        action_dir = self._action_bank_query
+	raise NotImplementedError
+	return bank
 
-    def query(self):
+    def _save_bank(self) -> None:
+	"""Save action bank to local directory.
+
+	Dump the action manifest to disk.
+	"""
+	raise NotImplementedError
+
+    def query(self, action_id: str) -> Action:
         """Return action information.
 
-        This runs .query on a Pandas DataFrame representation of the
-        actions in the action bank. 
+        This returns the action identified by the action id. 
 
+	Todo: Should this contain a secondary parameter to subset the action?
+
+	Parameters
+	----------
+	action_id: str
+	    The string identifier for the action.
+
+	Returns
+	-------
+	action: Action
+
+	Examples
+	--------
+	>>> steve = ActionBank()
+	>>> steve.query('actionid')
+	Representationofaction
         """
+	return self._action_manifest[action_id]
+
+    def _query(self, query_string: Optional[str] = None) -> pd.DataFrame:
+	"""Return filtered action set.
+
+	This constructs a Pandas DataFrame from the action manifest,
+	filters it according to the query_str parameter, and returns
+	the subset.
+
+	Parameters
+	----------
+	query_str: Optional[str] = None
+	    An empty string by default, this is a query string
+	    that meets the format used by pandas .query method.
+
+	Returns
+	-------
+	filtered_action_set: pandas.DataFrame
+	    This is the action manifest, filtered by a query string
+
+	Examples
+	--------
+	>>> # Here is code that makes a default manifest which
+	>>> #   has an 'accuracy' feature in the dataset and
+	>>> #   contains an action with an id of 'steve'.
+	>>> # There are *three* actions in this set, two of
+	>>> #   which have accuracy over 50%
+	>>> ab = ActionBank()
+	>>> act = ab._query('id=="steve"')
+	>>> isinstance(act,pd.DataFrame)
+	True
+	>>> act.nrow
+	1
+	>>> act_set = ab._query('accuracy>0.5')
+	>>> act_set.nrow
+	2
+	"""
+	action_frame = pd.DataFrame.from_dict(self._action_manifest)
+	return action_frame.query(query_str)
--- src/utils/types.py
+++ src/utils/types.py
@@ -1,11 +1,12 @@
 from ray import ObjectId
-from typing import Union, Iterable
+from typing import Dict, Iterable, Union
 
+# These types are used throughout the action bank
 ActionID = str
-Action = Union[ObjectId,ActionID] (inherited either from Ray or PyArrow, shouldn't matter)
-ActionSet = Iterable[Action]
+Action = Dict[str, Any]
+ActionSet = Dict[str, Action]
+
 ExperimentID = str
 Experiment = Union[ObjectId,ExperimentID]
-ActionBank: Iterable[Action]
 AgentID = str
 Agent = Union[ObjectId,AgentID]

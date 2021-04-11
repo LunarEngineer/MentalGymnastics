d49a01060c2150ec44e139722b71873e33d02fbf
timothy d luna <timothy.d.luna@gmail.com>
2021-04-11 14:05:08 -0500

    chore(container): prep for setup.py

--- docker_run.sh
+++ docker_run.sh
@@ -1,3 +1,5 @@
+
+#!/bin/bash
 docker build . -t mentalgymnastics
 # Example of docker command (note that this is overload cschranz)
 docker run --gpus all -d -it --rm -p 8888:8888 -v $(pwd):/home/jovyan/work -e GRANT_SUDO=yes -e JUPYTER_ENABLE_LAB=yes --name mentalgym_gpu --user root mentalgymnastics:latest

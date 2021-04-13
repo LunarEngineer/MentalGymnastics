b8cd8bc5d1b60a2d8a6b10bdb36bf04c078405c4
akutluozen-domino <akutluozen@dominomedia.com>
2021-04-12 22:15:07 -0400

    working on reward in step function

--- src/mentalgym/envs/mentalenv.py
+++ src/mentalgym/envs/mentalenv.py
@@ -3,6 +3,7 @@ import numpy as np
 from gym import error, spaces, utils
 from gym.utils import seeding
 from typing import Tuple
+from mentalgym.utils.reward import monotonic_reward, connection_reward, completion_reward
 
 class MentalEnv(gym.Env):
 	metadata = {'render.modes': ['human']}
@@ -14,11 +15,8 @@ class MentalEnv(gym.Env):
 		self.radius_max = self.experiment_space_max - self.experiment_space_min
 		self.number_functions = 8
 		self.number_metrics = 2
-		self.step_length = 10
+		self.max_steps = 10
 		self._step = 0
-		self.state = {
-			"experiment_space": (self.number_functions, ())
-		}
 
 		self.action_space = gym.spaces.Dict({
 			'function_id': spaces.Discrete(self.number_functions),
@@ -36,7 +34,8 @@ class MentalEnv(gym.Env):
 		
 		self.observation_space = gym.spaces.Dict({
 			'experiment_space': spaces.Tuple((
-				spaces.Discrete(self.number_functions), 
+				spaces.Discrete(self.number_functions), # This is the function ID
+				# This is the location
 				spaces.Box(
 					low=self.experiment_space_min, 
 					high=self.experiment_space_max, 
@@ -46,8 +45,7 @@ class MentalEnv(gym.Env):
 			'function_space': [0 for _ in range(self.number_functions)],
 			'function_metrics': np.ndarray((self.number_functions, self.number_metrics)) # populate with the actual actions
 		})
-	def step(self, action:Tuple[int, Tuple[float, float]]):		
-
+	def step(self, action:Tuple[int, Tuple[float, float, float]]):		
 		done = False
 		# create the state
 		# figure out the next state
@@ -57,9 +55,11 @@ class MentalEnv(gym.Env):
 		# else:
 		# 	reward = -1
 
-		self.state[self._step] = action
+		# monotonic_reward(self.observation_space["experiment_space"], action, )
+
+		self.observation_space[self._step] = action
 		
-		if self._step == self.step_length:
+		if self._step == self.max_steps:
 			done = True
 
 		self._step += 1

c697021419da9def254305b2b1f3fc0ef5f98218
akutluozen-domino <akutluozen@dominomedia.com>
2021-04-12 22:59:56 -0400

    pseudo code for step function, new details on observation space

--- src/mentalgym/envs/mentalenv.py
+++ src/mentalgym/envs/mentalenv.py
@@ -31,33 +31,47 @@ class MentalEnv(gym.Env):
 				shape=(1,)
 			)
 		})
-		
+
 		self.observation_space = gym.spaces.Dict({
-			'experiment_space': spaces.Tuple((
-				spaces.Discrete(self.number_functions), # This is the function ID
+			'experiment_space': spaces.Dict({
+				# This needs to be an iterable as long as the episode length 
+				# where each element can have a value from 0 to the number of functions in the function bank
+				'function_ids': spaces.Discrete(self.max_steps), 
 				# This is the location
-				spaces.Box(
+				'function_locations': spaces.Box(
 					low=self.experiment_space_min, 
 					high=self.experiment_space_max, 
-					shape=(2,)
-				)
-			)),
+					shape=(self.max_steps,)
+				),
+				# This is 1 if a connection is made
+				'function_connections': spaces.MultiBinary(1) 
+			}),
 			'function_space': [0 for _ in range(self.number_functions)],
 			'function_metrics': np.ndarray((self.number_functions, self.number_metrics)) # populate with the actual actions
 		})
 	def step(self, action:Tuple[int, Tuple[float, float, float]]):		
 		done = False
-		# create the state
-		# figure out the next state
+		
+		# get all source and sink nodes from the function bank
+		# get the function representation from the function bank
 
-		# if action == 2:
-		# 	reward = 1
-		# else:
-		# 	reward = -1
+		# determine if that is a atomic or composed function
+		# if atomic
+		#	determine all input notes using radius and other nodes locations
+		# else
+		#	just recreate as it was placed
+
+		# When building the net, don't hook on output layer until done
+		# net is build at this point
+		
+		# if reached maximum steps (when done) or if connected to the sink
+		# 	run and measure
+		
+		# calculate reward based on current observation space and 
 
 		# monotonic_reward(self.observation_space["experiment_space"], action, )
 
-		self.observation_space[self._step] = action
+		self.observation_space['experiment_space']
 		
 		if self._step == self.max_steps:
 			done = True

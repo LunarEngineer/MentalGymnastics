057eacc53ba1a13d0194af00aee64eb7eb437d61
Vahe Hagopian <vahe.hagopian@gmail.com>
2021-04-21 18:45:56 -0700

    Changed agent and env over to stable baselines

--- src/agent.py
+++ src/agent.py
@@ -1,12 +1,16 @@
-# import gym
-import random
 import numpy as np
 import torch
 import mentalgym
 import mentalgym.envs
 import mentalgym.functionbank
 import agentNets
-from collections import deque
+import gym
+
+from stable_baselines3 import A2C
+from stable_baselines3.common.env_util import make_vec_env
+
+from stable_baselines3.common.env_checker import check_env
+
 
 
 class MentalAgent:
@@ -18,160 +22,22 @@ class MentalAgent:
         # Instantiate environment
         self.env = mentalgym.envs.MentalEnv()
 
-        # Create DQN NN
-        self.DQN_agent_Q = agentNets.DQNAgentNN(hparams)
+        #  It will check your custom environment and output additional warnings if needed
+        check_env(self.env)
 
-        # Instantiate loss criterion
-        self.criterion = torch.nn.MSELoss()
-
-        # Instantiate Optimizer
-        self.optimizer = torch.optim.Adam(
-            self.DQN_agent_Q.parameters(), lr=self.alpha
-        )
-
-        # Mask for invalid functions
-        self.num_active_fns = hparams["num_active_fns_init"]
-
-        # Experience replay buffer
-        self.replay_buffer = deque(maxlen=hparams["buffer_len"])
-
-        # Variable to store all the episodic rewards
-        self.rewards = np.zeros(hparams["num_episodes"])
+        # Create A2C Agent
+#        policy_kwargs = dict(act_fun=tf.nn.tanh, net_arch=[32, 32])
+        self.model = A2C("MlpPolicy", self.env, verbose=1,
+                         gamma=hparams["gamma"],
+                         learning_rate=hparams["alpha_start"])
+#                         policy_kwargs)
 
     def train(self, hparams):
-        # Iterate over episodes
-        for e in range(hparams["num_episodes"]):
-            # Initialize state
-            S = self.env.reset()
-
-            # Accumulate reward for each episode
-            dqn_reward = 0
-
-            # Count time steps for each episode
-            t = 0
-
-            # Flag to detect episode termination
-            done = False
-
-            # Iterate over time steps in an episode
-            while not done:
-                t += 1
-
-                # Vectorize state by changing function IDs into one-hot vectors
-                fns_oh = np.zeros(
-                    (hparams["max_steps"], hparams["num_functions"])
-                )
-                for i in range(hparams["max_steps"]):
-                    fns_oh[
-                        np.arange(hparams["max_steps"]),
-                        S["experiment_space"]["function_ids"][i],
-                    ] = 1
-
-                dqn_S = torch.cat(
-                    (
-                        torch.Tensor(fns_oh.flatten()),
-                        torch.Tensor(
-                            S["experiment_space"]["function_locations"]
-                        ),
-                        torch.Tensor(
-                            [S["experiment_space"]["function_connection"]]
-                        ),
-                    )
-                )
-
-                # Obtain action values
-                with torch.no_grad():
-                    dqas = self.DQN_agent_Q(dqn_S.reshape(1, len(dqn_S)))
-
-                # Behavior Policy: epsilon-greedy
-                if np.random.rand() < self.epsilon:
-                    dqn_action = np.random.randint(
-                        hparams["num_active_fns_init"]
-                    )
-                else:
-                    dqn_action = np.argmax(dqas[0])
-
-                # TODO: need to supply location and radius components of action
-                action = {
-                    "function_id": dqn_action,
-                    "location": (0, 0),
-                    "radius": 0,
-                }
-
-                # Take a step in the environment
-                Sp, dqn_R, done, info = self.env.step(action)
-
-                # Accumulate episodic reward
-                dqn_reward += dqn_R
-
-                # Vectorize next state by changing function IDs into
-                # one-hot vectors
-                fnsp_oh = np.zeros(
-                    (hparams["max_steps"], hparams["num_functions"])
-                )
-                for i in range(hparams["max_steps"]):
-                    fnsp_oh[
-                        np.arange(hparams["max_steps"]),
-                        Sp["experiment_space"]["function_ids"][i],
-                    ] = 1
-
-                dqn_Sp = np.concatenate(
-                    (
-                        fnsp_oh.flatten(),
-                        Sp["experiment_space"]["function_locations"],
-                        [Sp["experiment_space"]["function_connection"]],
-                    )
-                )
-
-                # Append experience to replay buffer
-                self.replay_buffer.append(
-                    (dqn_S.numpy(), dqn_action, dqn_R, dqn_Sp, done)
-                )
-
-                # Sample minibatch from buffer if buffer is filled enough
-                if len(self.replay_buffer) > hparams["min_buffer_use_size"]:
-                    minibatch = np.array(
-                        random.sample(
-                            self.replay_buffer, hparams["minibatch_size"]
-                        ),
-                        dtype=object,
-                    )
-                    Xr = torch.Tensor(
-                        np.stack(np.concatenate(minibatch[:, 0:1]))
-                    )
-                    Ar = torch.tensor(
-                        np.array(minibatch[:, 1:2], dtype=int),
-                        dtype=torch.long,
-                    )
-                    Rr = torch.Tensor(np.array(minibatch[:, 2:3], dtype=float))
-                    Spr = torch.Tensor(
-                        np.stack(np.concatenate(np.array(minibatch[:, 3:4])))
-                    )
-                    doner = torch.tensor(
-                        np.array(minibatch[:, 4:5], dtype=int),
-                        dtype=torch.int32,
-                    )
-
-                    # Train DQN NN
-                    self.optimizer.zero_grad()
-                    target = (
-                        Rr
-                        + hparams["gamma"]
-                        * torch.max(self.DQN_agent_Q(Spr), 1, keepdim=True)[0]
-                    )
-                    target = (1 - doner) * target + doner * Rr
-                    Y = self.DQN_agent_Q(Xr)
-                    Yt = Y.clone()
-                    Yt[torch.arange(0, hparams["minibatch_size"]), Ar] = target
-                    loss = self.criterion(Y, Yt)
-                    loss.backward()
-                    self.optimizer.step()
-
-                # Advance state
-                S = Sp
-
-                # Print Status
-                print("Episode:", e + 1, "Time step:", t)
+        obs = self.env.reset()
+        while True:
+            action, _states = self.model.predict(obs)
+            obs, rewards, dones, info = self.env.step(action)
+            self.env.render()
 
 
 if __name__ == "__main__":
@@ -195,6 +61,3 @@ if __name__ == "__main__":
 
     agent = MentalAgent(hparams)
     agent.train(hparams)
-
-#    function_bank = mentalgym.functionbank.FunctionBank("mentalgym/functions")
-#    print(function_bank)
--- src/mentalgym/envs/mentalenv.py
+++ src/mentalgym/envs/mentalenv.py
@@ -1,6 +1,6 @@
 import gym
 
-# import numpy as np
+import numpy as np
 from gym import (
     # error,
     spaces,
@@ -21,6 +21,8 @@ class MentalEnv(gym.Env):
     metadata = {"render.modes": ["human"]}
 
     def __init__(self):
+        super(MentalEnv, self).__init__()
+
         self.experiment_space_min = -100
         self.experiment_space_max = 100
         self.radius_min = 1
@@ -29,47 +31,58 @@ class MentalEnv(gym.Env):
         self.number_metrics = 2
         self.max_steps = 10
         self._step = 0
+        self.state = self.reset()
 
-        self.action_space = gym.spaces.Dict(
-            {
-                "function_id": spaces.Discrete(self.number_functions),
-                "location": spaces.Box(
-                    low=self.experiment_space_min,
-                    high=self.experiment_space_max,
-                    shape=(2,),
-                ),
-                "radius": spaces.Box(
-                    low=self.radius_min, high=self.radius_max, shape=(1,)
-                ),
-            }
-        )
+        self.action_space = gym.spaces.Box(4,)
+        # self.action_space = gym.spaces.Dict(
+            # {
+                # "function_id": spaces.Discrete(self.number_functions),
+                # "location": spaces.Box(
+                    # low=self.experiment_space_min,
+                    # high=self.experiment_space_max,
+                    # shape=(2,),
+                # ),
+                # "radius": spaces.Box(
+                    # low=self.radius_min, high=self.radius_max, shape=(1,)
+                # ),
+            # }
+        # )
+        # self.action_space = spaces.Tuple([spaces.Discrete(1)])
 
         # Create iterable for function IDs
-        fn_ids_tuple = ()
-        for i in range(self.max_steps):
-            fn_ids_tuple += (spaces.Discrete(self.number_functions),)
+#        fn_ids_tuple = ()
+#        for i in range(self.max_steps):
+#            fn_ids_tuple += (spaces.Discrete(self.number_functions),)
 
-        self.observation_space = gym.spaces.Dict(
-            {
-                "experiment_space": spaces.Dict(
-                    {
-                        # Functions currently placed in the experiment space
-                        "function_ids": spaces.Tuple(fn_ids_tuple),
-                        # Locations of those functions
-                        "function_locations": spaces.Box(
-                            low=self.experiment_space_min,
-                            high=self.experiment_space_max,
-                            shape=(self.max_steps,),
-                        ),
-                        # This is 1 if a connection is made
-                        "function_connection": spaces.MultiBinary(1),
-                    }
-                ),
-                #  "function_metrics": np.ndarray(
-                #      (self.number_functions, self.number_metrics)
-                #  ),  # populate with the actual actions
-            }
-        )
+        # Fn ID, locX, locY, FnConnection
+        self.observation_space = gym.spaces.Box(4,self.max_steps)
+
+
+        # self.observation_space = gym.spaces.Dict(
+            # {
+
+
+
+                # "experiment_space": spaces.Dict(
+                    # {
+                        # # Functions currently placed in the experiment space
+                        # "function_ids": spaces.Tuple(fn_ids_tuple),
+                        # # Locations of those functions
+                        # "function_locations": spaces.Box(
+                            # low=self.experiment_space_min,
+                            # high=self.experiment_space_max,
+                            # shape=(self.max_steps,),
+                        # ),
+                        # # This is 1 if a connection is made
+                        # "function_connection": spaces.MultiBinary(1),
+                    # }
+                # ),
+                # #  "function_metrics": np.ndarray(
+                # #      (self.number_functions, self.number_metrics)
+                # #  ),  # populate with the actual actions
+            # }
+        # )
+        # self.observation_space = spaces.Tuple([spaces.Discrete(1)])
 
     #   def step(self, action:Dict[int, Any]):
     def step(self, action):
@@ -92,32 +105,33 @@ class MentalEnv(gym.Env):
 
         # calculate reward based on current observation space and
 
-        # monotonic_reward(
-        #     self.observation_space["experiment_space"],
-        #     action,
-        # )
-
-        self.observation_space["experiment_space"]
-
+        print("Action:", action)
         self._step += 1
 
         if self._step == self.max_steps:
             done = True
 
         info = {}
-        self.state = {
-            "experiment_space": {
-                "function_ids": tuple([0] * self.max_steps),
-                "function_locations": [0] * self.max_steps,
-                "function_connection": 0,
-            }
-        }
+
+        # TODO: Check if new function connects to ANY node
+#        for
+
+#        self.state["experiment_space"]["function_ids"] += (action["function_id"],)
+#        self.state["experiment_space"]["function_locations"] += (action["location"],)
+
+#        if not self.function_connection:
+#            if distance(input, action["location"]
+#            if action['radius']
+
         reward = 0
 
         return self.state, reward, done, info
 
     def reset(self):
         self._step = 0
+        self.function_connection = False
+
+        # TODO: Initialize state to Input (get ID for this input to query in .step())
         state = {
             "experiment_space": {
                 "function_ids": tuple([0] * self.max_steps),
@@ -125,6 +139,7 @@ class MentalEnv(gym.Env):
                 "function_connection": 0,
             }
         }
+        state = np.zeros(3)
         return state
 
     def render(self, mode="human"):
